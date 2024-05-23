4a5f234110ff4e506335b6c2bc90050d0c322f98
Dann . <zadanfaidelta@gmail.com>
2024-05-23 13:42:23 +0700

    project finished

---
+++ app/js/script.js
@@ -0,0 +1,56 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+const setColorMode = () => {
+    
+    if(localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode()
+        darkButton.click();
+    } else if((localStorage.getItem('colorMode') == 'light')){ 
+        setLightMode();
+        lightButton.click();
+    }
+};
+
+const checkMode = () => {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        lightButton.click();
+    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        darkButton.click();
+    }
+};
+
+const checkModeChange = () => {
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (event) => {
+            checkMode();
+        })
+}
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark';
+};
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+
+
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener('click', (event) => {
+        if (darkButton.checked) {
+            localStorage.setItem('colorMode', 'dark');
+            setDarkMode();
+        } else {
+            localStorage.setItem('colorMode', 'light');
+            setLightMode();
+        }
+    });
+}
+
